70b8bc4387bd723050aa5ac6b7964651fab5e3bf
Sqwack-Studios <transistordaniel10@gmail.com>
2025-09-07 20:56:12 +0200

    add jumbo file for convenience

---
+++ imgui_jumbo.cpp
@@ -0,0 +1,23 @@
+
+#ifndef _IMGUI_JUMBO_CPP_
+#define _IMGUI_JUMBO_CPP_
+
+#define IMGUI_DEFINE_MATH_OPERATORS
+
+#include "imgui.h"
+#include "imgui_internal.h"
+#include "backends/imgui_impl_win32.h"
+#include "backends/imgui_impl_dx12.h"
+
+
+#include "imgui_draw.cpp"
+#include "imgui_tables.cpp"
+#include "imgui_widgets.cpp"
+#include "imgui.cpp"
+#include "backends/imgui_impl_win32.cpp"
+#include "backends/imgui_impl_dx12.cpp"
+
+
+
+
+#endif //_IMGUI_JUMBO_CPP_
